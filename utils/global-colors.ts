a93b4d4bd8aa6ae9deba54f35e98aaf50882627a
Zerodayu <kimianalconera39@gmail.com>
2025-12-01 21:54:44 +0800

    update: theme colors

---
+++ utils/global-colors.ts
@@ -0,0 +1,44 @@
+
+const dark = {
+  primary: "#3b82f6",
+  primaryForeground: "#ffffff",
+  secondary: "#262626",
+  secondaryForeground: "#e5e5e5",
+
+  accent: "#1e3a8a",
+  accentForeground: "#bfdbfe",
+
+  background: "#171717",
+  foreground: "#e5e5e5",
+  destructive: "#ef4444",
+}
+
+const light = {
+  primary: "#3b82f6",
+  primaryForeground: "#ffffff",
+  secondary: "#f3f4f6",
+  secondaryForeground: "#4b5563",
+
+  accent: "#e0f2fe",
+  accentForeground: "#1e3a8a",
+
+  background: "#ffffff",
+  foreground: "#333333",
+  destructive: "#ef4444",
+}
+
+const theme = dark;
+
+export const globalColors = {
+  primary: theme.primary,
+  primaryForeground: theme.primaryForeground,
+  secondary: theme.secondary,
+  secondaryForeground: theme.secondaryForeground,
+
+  accent: theme.accent,
+  accentForeground: theme.accentForeground,
+
+  background: theme.background,
+  foreground: theme.foreground,
+  destructive: theme.destructive,
+}
