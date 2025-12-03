3b0b6beb8766865f430480664299f1e28d99a20e
Zerodayu <kimianalconera39@gmail.com>
2025-12-03 19:31:55 +0800

    update: topnav style

--- mobile-app/app/(tabs)/(stats)/_layout.tsx
+++ mobile-app/app/(tabs)/(stats)/_layout.tsx
@@ -1,23 +1,57 @@
 import { Tabs, TabList, TabTrigger, TabSlot } from 'expo-router/ui';
-import { Text } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { useSegments } from 'expo-router';
+import { mainColors } from '@/utils/global-theme';
 
 
 export default function Layout() {
   const segments = useSegments();
   const activeTab = segments[segments.length - 1];
-  
+
   return (
-     <Tabs style={{ flex: 1, height: '20%' }}>
-      <TabSlot />
-      <TabList>
-        <TabTrigger name="home" href="/">
-          <Text style={{ color: activeTab === '(stats)' ? 'red' : 'black' }}>Home</Text>
+    <Tabs style={{ flex: 1, height: '20%' }}>
+      <TabList style={{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: mainColors.accent,
+      }}>
+        <TabTrigger name="home" href="/" asChild>
+          <TouchableOpacity style={{
+            borderBottomWidth: activeTab === '(stats)' ? 2 : 0,
+            borderBottomColor: activeTab === '(stats)' ? mainColors.primary : mainColors.background,
+            width: "50%",
+            paddingVertical: 10,
+            alignContent: 'center',
+            justifyContent: 'center'
+          }}>
+            <Text style={{
+              fontWeight: activeTab === '(stats)' ? "bold" : "normal"
+            }}>Home</Text>
+          </TouchableOpacity>
         </TabTrigger>
-        <TabTrigger name="article" href="/temp">
-          <Text style={{ color: activeTab === 'temp' ? 'red' : 'black' }}>Temp</Text>
+        <TabTrigger name="temp" href="/temp" asChild>
+          <TouchableOpacity style={{
+            borderBottomWidth: activeTab === 'temp' ? 2 : 0,
+            borderBottomColor: activeTab === 'temp' ? mainColors.primary : mainColors.background,
+            width: "50%",
+            paddingVertical: 10,
+            alignContent: 'center',
+            justifyContent: 'center'
+          }}>
+            <Text style={{
+              fontWeight: activeTab === 'temp' ? "bold" : "normal"
+            }}>Temp</Text>
+          </TouchableOpacity>
         </TabTrigger>
       </TabList>
+      <TabSlot />
     </Tabs>
   )
 }
+
+const styles = StyleSheet.create({
+  tabBtn: {
+    width: 100,
+    paddingVertical: 10
+  }
+})
